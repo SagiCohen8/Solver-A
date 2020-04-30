e04a55c1e9da500503d1340ef0518a3d5ab04b85
SagiCohen8 <sagic8@gmail.com>
2020-04-30 22:28:10 +0300

    first commit

---
+++ solver.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+namespace solver
+{
+    double solve(RealVariable real);
+    std::complex<double> solve(ComplexVariable cmplx);
+    class RealVariable
+    {
+
+    };
+    class ComplexVariable
+    {
+
+    };
+};
