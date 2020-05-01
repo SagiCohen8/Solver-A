c2fdce8c3ec3e5aa7045966123364d05fc39dfd0
orad-cohen <orad37@gmail.com>
2020-04-30 20:08:24 -0400

    finishing touches

--- solver.cpp
+++ solver.cpp
@@ -1,1 +1,66 @@
 #include "solver.hpp"
+#include <iostream>
+#include <stdio.h>
+#include<complex>
+using namespace std;
+using namespace solver;
+
+solver::RealVariable T;
+double solver::solve(const RealVariable &r){
+
+    return 1.0;
+
+}
+complex<double> solver::solve(const ComplexVariable &cmplx){
+
+    return complex<double> (3);
+}
+const solver::RealVariable& solver::operator*(int r, const RealVariable &x)
+{
+    return T;
+}
+
+const solver::RealVariable& solver::operator==(const solver::RealVariable& x, int r)
+{
+    return T;
+}
+
+const solver::RealVariable& solver::operator^(const solver::RealVariable& x, int r){
+    return T;
+}
+const solver::RealVariable& solver::operator-(const solver::RealVariable& x, int d){
+    return T;
+}
+const solver::RealVariable& solver::operator+(const solver::RealVariable& x1, double d1){
+    return T;
+}
+const solver::RealVariable& solver::operator+(const solver::RealVariable&  x1, const solver::RealVariable& x2){
+    return T;
+}
+const solver::RealVariable& solver::operator+(int d1, const solver::RealVariable& x2){
+    return T;
+}
+const solver::RealVariable& solver::operator/(const solver::RealVariable& x1, double d1){
+    return T;
+}
+const solver::RealVariable& solver::operator/(const solver::RealVariable& x1, int d1){
+    return T;
+}
+const solver::RealVariable& solver::operator-(const solver::RealVariable& x1, const solver::RealVariable& d1){
+    return T;
+}
+const solver::RealVariable& solver::operator==(const solver::RealVariable& x1, const solver::RealVariable& x2){
+    return T;
+}
+ComplexVariable C;
+const solver::ComplexVariable& solver::operator*(int r, const ComplexVariable &x){ return C;}
+const solver::ComplexVariable& solver::operator^(const ComplexVariable &r1, int r2){ return C;}
+const solver::ComplexVariable& solver::operator/(const ComplexVariable &r1, int r2){ return C;}
+const solver::ComplexVariable& solver::operator-(const ComplexVariable &r1, const ComplexVariable &r2){ return C;}
+const solver::ComplexVariable& solver::operator-(const ComplexVariable &r1, int r2){ return C;}
+const solver::ComplexVariable& solver::operator==(const ComplexVariable &r1, double r2){ return C;}
+const solver::ComplexVariable& solver::operator==(const ComplexVariable &r1, const ComplexVariable &r2){ return C;}
+const solver::ComplexVariable& solver::operator+(const ComplexVariable &r1, const ComplexVariable &r2){ return C;}
+const solver::ComplexVariable& solver::operator+(const ComplexVariable &r1, int r2){ return C;}
+const solver::ComplexVariable& solver::operator+(int r2, const ComplexVariable &r1){ return C;}
+const solver::ComplexVariable& solver::operator+(const ComplexVariable &r1, complex<double> r2){ return C;}
--- solver.hpp
+++ solver.hpp
@@ -1,16 +1,77 @@
 #include <iostream>
+#include <stdio.h>
+#include <complex>
 using namespace std;
+
+
+
 
 namespace solver
 {
-    double solve(RealVariable real);
-    std::complex<double> solve(ComplexVariable cmplx);
+    
+    
     class RealVariable
     {
+        
+        public:
+            
+            RealVariable(){
 
+            }
+
+        
+        
+            
+            
+             
     };
+    const RealVariable &operator*(int r, const RealVariable &x);
+    const RealVariable &operator==(const RealVariable &x, int r);
+    const RealVariable &operator-(const RealVariable &r, int d);
+    const RealVariable &operator^(const RealVariable &x, int r);
+
+    const RealVariable &operator+(const RealVariable &r1, double d1);
+    const RealVariable &operator+(const RealVariable &r1, const RealVariable &r2);
+    const RealVariable &operator+(int d1, const RealVariable &r2);
+    const RealVariable &operator/(const RealVariable &r1, double d1);
+    const RealVariable &operator/(const RealVariable &r1, int d1);
+    const RealVariable &operator-(const RealVariable &r1, const RealVariable &d1);
+    const RealVariable &operator==(const RealVariable &r1, const RealVariable &r2);
+
     class ComplexVariable
     {
+        double _re,_im;
 
+        public:
+            ComplexVariable(){
+
+            }
+            ostream &operator<<(ostream &os) const
+            {
+                os << "(" << _re << "+" << _im << "i)";
+                return os;
+            }
+            
     };
-};
+
+    const ComplexVariable &operator*(int r, const ComplexVariable &x);
+    const ComplexVariable &operator^(const ComplexVariable &r1, int r2);
+    const ComplexVariable &operator/(const ComplexVariable &r1, int r2);
+    const ComplexVariable &operator-(const ComplexVariable &r1, const ComplexVariable &r2);
+    const ComplexVariable &operator-(const ComplexVariable &r1, int r2);
+    const ComplexVariable &operator==(const ComplexVariable &r1, double r2);
+    const ComplexVariable &operator==(const ComplexVariable &r1, const ComplexVariable &r2);
+    const ComplexVariable &operator+(const ComplexVariable &r1, const ComplexVariable &r2);
+    const ComplexVariable &operator+(const ComplexVariable &r1, int r2);
+    const ComplexVariable &operator+(int r2, const ComplexVariable &r1);
+    const ComplexVariable &operator+(const ComplexVariable &r1, complex<double> r2);
+
+    complex<double> solve(const ComplexVariable &cmplx);
+    double solve(const RealVariable &r);
+        
+    
+
+
+    
+    
+}
