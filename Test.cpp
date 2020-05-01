7a7c32ad0c48f2fc5575f6fc0ef7671ec4bf2ad1
orad-cohen <orad37@gmail.com>
2020-05-01 01:04:23 -0400

    tests

--- Test.cpp
+++ Test.cpp
@@ -8,11 +8,27 @@ using namespace solver;
 
 
 
-TEST_CASE(""){
+TEST_CASE("linear equations"){
 
 
     int i;
     RealVariable x;
+    CHECK(solve(3*x-12==0) == 4);
+    CHECK(solve(2*x + 12 == 48) ==18);
+    CHECK(solve(6*x-13 == 8+5*x) ==21);
+    CHECK(solve() ==);
+    CHECK(solve() ==);
+    CHECK(solve() ==);
+    CHECK(solve() ==);
+    CHECK(solve() ==);
+    CHECK(solve() ==);
+    CHECK(solve() ==);
+    CHECK(solve() ==);
+    CHECK(solve() ==);
+    CHECK(solve() ==);
+    CHECK(solve() ==);
+    CHECK(solve() ==);
+
     for ( i = 0; i < 100; i++)
     {
         CHECK(solve(x^2)==7.0);
